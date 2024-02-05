bee1b9320a47984d41135cc558c25a0c50227a79
unknown <98gmgomez@gmail.com>
2024-02-05 20:13:44 -0300

    Resolvi ejercicios

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Gabiel
+apellido: Gomez
 ---
 Ejercicio: entrada_salida_02
 ---
@@ -27,7 +27,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre = prompt(title="UTN",prompt="Ingrese su nombre:")
+        alert(title="UTN",message= ("Hola",nombre,"!"))
         
         
     
--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_04.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_04.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Gabriel
+apellido: Gomez
 ---
 Ejercicio: entrada_salida_04
 ---
@@ -33,7 +33,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre = prompt(title="UTN FRA",prompt="Ingrese su nombre:")
+        self.txt_nombre.delete(0,100)
+        self.txt_nombre.insert(0,nombre)
         
     
 if __name__ == "__main__":
